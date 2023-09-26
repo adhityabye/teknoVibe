70d3d4e42a0fe79438310167208e43f65c03a6ea
leleonnn <leonardmars63@gmail.com>
2023-09-26 15:29:45 +0700

    add event model

---
+++ backend/model/event.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  eventName: { type: String, required: true },
+  eventDescription: { type: String, required: true },
+  date: { type: Date, required: true, default: Date.now() },
+  divisions: { type: String, required: true },
+  deadlineDate: { type: Date, required: true },
+  tnc: { type: String, required: true },
+});
+
+const eventModel = mongoose.model("event", eventSchema);
+module.exports = eventModel;
