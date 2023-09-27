91585a049c782550b2b1861e9824c58a43343496
defz7 <daffa.kamal@mail.ugm.ac.id>
2023-09-27 20:36:25 +0700

    feat: define user registration, login, and user listing routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,47 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../model/user');
-const bcrypt = require('bcrypt');
+const { registerUser, getUsers, loginUser } = require('../controller/userController');
 
-router.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
+// Define the route for user registration
+router.post('/register', registerUser);
 
-  // Check if all required fields are filled
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: 'All fields are required' });
-  }
+// Define the route for user login
+router.post('/login', loginUser);
 
-  // Check if the password length is less than 8 characters
-  if (password.length < 8) {
-    return res.status(400).json({ message: 'Password must be at least 8 characters long' });
-  }
-
-  try {
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 6);
-
-    // Create a new user document
-    const newUser = new userModel({ name, email, password: hashedPassword });
-
-    // Save the user to the database
-    await newUser.save();
-
-    // Respond with a success message
-    return res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-router.get('/users', async (req, res) => {
-  try {
-    const users = await userModel.find();
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+// Define the route for listing users
+router.get('/users', getUsers);
 
 module.exports = router;
