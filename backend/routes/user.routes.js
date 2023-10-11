77cc0096917116eac7c3b2abbf27b33caa514cef
defz-07 <93387102+wannabegooddevv@users.noreply.github.com>
2023-10-11 14:57:04 +0700

    feat: add update user route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, getUsers, loginUser, deleteUser } = require('../controller/user.controller');
+const { registerUser, getUsers, loginUser, updateUser, deleteUser } = require('../controller/user.controller');
 
 // Define the route for user registration
 router.post('/register', registerUser);
@@ -13,5 +13,8 @@ router.get('/users', getUsers);
 
 // Define the route for deleting user
 router.delete('/users/:id', deleteUser);
+
+// Define the route for updating user data
+router.put('/users/:id', updateUser);
 
 module.exports = router;
