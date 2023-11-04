cba51d81127277a9c3f2208cf923b6d1af2c6030
defz7 <daffa.kamal@mail.ugm.ac.id>
2023-11-04 13:25:56 +0700

    feat: add not-found page

---
+++ frontend/src/app/not-found.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import CircleBackground from "./components/circleBackground";
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen w-full relative mx-auto grid justify-items-center items-center px-6 overflow-hidden">
+      <div className="z-30 space-y-2 text-center">
+        <h1 className="font-bold text-2xl sm:text-[28px]">
+          404 - Halaman Tidak Ditemukan
+        </h1>
+        <p className="font-normal text-base sm:text-lg">
+          Halaman yang Anda cari tidak ada. Silahkan kembali ke Beranda
+        </p>
+        <Link href="/">
+          <button className="px-4 py-1.5 mt-4 bg-purple-900 text-white-100 text-sm rounded-2xl">
+            Beranda
+          </button>
+        </Link>
+        <CircleBackground />
+      </div>
+    </div>
+  );
+}
