889bc86469d9f240ed6495b7b26316eca653a872
KhairunNisaZ <khairun.nisa.zuqri92@gmail.com>
2023-11-09 16:29:08 +0700

    fix: restructure folder, update about and hero section

---
+++ frontend/src/app/components/home/Hero.jsx
@@ -0,0 +1,11 @@
+export default function Hero(){
+    return(
+      <main>
+        <div>
+            <div>
+                
+            </div>
+        </div>
+      </main>
+    );
+  }
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/home/Hero"
 
 export default function Home(){
   return(
