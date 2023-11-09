bfc40ae3cb864dc64cd2124d330ac8d99d9f8d4b
KhairunNisaZ <khairun.nisa.zuqri92@gmail.com>
2023-11-09 23:50:58 +0700

    feat: add content in about and instruction page

--- frontend/src/app/components/home/Hero.jsx
+++ frontend/src/app/components/home/Hero.jsx
@@ -4,7 +4,7 @@ import Wave from '../../../../public/assets/wave.svg'
 
 const Button = ({title}) => {
   return(
-    <button className="bg-purple-900 text-white-100 px-5 py-2.5 text-sm rounded-md active:scale-95">{title}</button>
+    <button className="bg-purple-900 text-white-100 px-5 py-2.5 text-sm rounded-md z-10 active:scale-95">{title}</button>
   )
 }
 
@@ -13,10 +13,10 @@ export default function Hero(){
       <main className="bg-purple-200 w-full">
         <div className='flex flex-col items-center'>
           <div className='flex flex-row w-[75rem]'>
-            <div className='w-full md:w-1/2 flex flex-col gap-4  justify-center'>
+            <div className='w-full md:w-1/2 flex flex-col gap-4 justify-center'>
               <h1 className="text-white-100 text-4xl font-semibold">Cari dan Ajukan Event untuk Seluruh Mahasiswa FT UGM</h1>
               <p className="text-white-100 text-sm">Temukan dan daftarkan event-event di lingkungan Fakultas Teknik UGM dengan mudah dan terintegrasi!</p>
-              <div className="flex flex-row gap-4">
+              <div className="relative flex flex-row gap-4">
                 <Button title={"Cari Event"}/>
                 <Button title={"Ajukan Event"}/>
               </div>
--- frontend/src/app/panduan/page.jsx
+++ frontend/src/app/panduan/page.jsx
@@ -1,16 +1,39 @@
 export default function Panduan(){
     return(
       <main id="panduan">
-        <h1>Panduan</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet eu tortor ut lobortis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed accumsan mi vitae libero tristique sagittis vitae ac dui. Vestibulum sit amet posuere diam, sit amet blandit mi. Aliquam vitae turpis blandit, bibendum neque ac, luctus risus. Fusce dictum tortor at magna ultrices, pulvinar rhoncus leo varius. Cras tincidunt elementum libero malesuada vulputate. Phasellus dapibus mi enim, nec eleifend est egestas vitae. Praesent at tellus turpis. Nunc gravida dapibus urna id pretium. Quisque quis mauris molestie, finibus ante in, dignissim velit. Aenean at lacinia lacus, at mattis arcu.
-
-Fusce eget urna venenatis, venenatis purus sit amet, mollis felis. Aenean vehicula risus ut diam laoreet vulputate. In scelerisque sapien suscipit dui tempor tempus. Aenean consectetur rhoncus quam sed accumsan. Nulla lectus quam, gravida at semper ac, vulputate a neque. Suspendisse posuere arcu feugiat ipsum vestibulum, at luctus ligula pharetra. Donec fringilla feugiat dui eu accumsan. Nunc at libero eu purus ornare rhoncus. Cras condimentum lacus nisl, sit amet varius erat molestie nec. Morbi suscipit lacus nisl, sit amet pharetra nulla condimentum nec. Morbi ornare, felis nec consequat congue, erat odio commodo velit, id porttitor ligula eros eu erat. Proin aliquam tincidunt diam non lobortis. Morbi vitae fringilla tortor. Integer at luctus velit. Fusce efficitur gravida urna, sit amet placerat urna suscipit at. Nullam rhoncus vulputate sapien, et convallis libero venenatis eget.
-
-Nam ipsum sem, commodo non maximus id, dignissim sit amet massa. Nullam auctor tincidunt condimentum. Sed tincidunt hendrerit lacus id porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris faucibus lectus in velit pharetra scelerisque. Sed maximus, elit finibus varius scelerisque, risus tellus semper velit, sed sodales dui magna eget nunc. Fusce tincidunt enim tortor, quis maximus lectus tincidunt sed. Maecenas ut lobortis ligula. Aenean auctor a lorem sit amet auctor. Praesent laoreet sem hendrerit mollis mattis. In hac habitasse platea dictumst. Nam nec nunc eu justo congue pellentesque. Integer convallis dignissim urna sed lobortis. Aliquam vitae sodales lorem. Curabitur fringilla tellus nec lectus mattis consectetur. Proin a ligula enim.
-
-Praesent rhoncus consectetur dui, sed tincidunt tortor convallis ac. Praesent a lorem nec mauris iaculis scelerisque. Quisque fermentum elit non orci varius imperdiet. Aenean posuere tortor turpis, nec feugiat lectus hendrerit eu. Donec semper ex sit amet dignissim viverra. Ut pharetra nibh vitae consequat egestas. Donec egestas nibh sed mi euismod, et sodales risus tristique. Donec tempus lorem nec mauris porttitor, quis pretium mauris bibendum. Fusce et velit sed felis accumsan feugiat. Ut scelerisque massa sed consectetur sollicitudin. Morbi venenatis ultricies ante, nec commodo ipsum suscipit quis. Cras vel porttitor ex. Sed efficitur interdum quam, non feugiat urna elementum sit amet.
-
-In laoreet ac dui ut eleifend. Quisque eget erat eget diam convallis faucibus feugiat non purus. In finibus laoreet est, ut venenatis nisi ullamcorper nec. Nam tincidunt enim ante, eu facilisis mi accumsan sit amet. Duis nec vehicula turpis. Fusce ut faucibus urna, non sollicitudin orci. Ut vel ante vitae ante finibus commodo suscipit eget nunc. Nam a libero vitae mi bibendum hendrerit. Quisque mattis sapien ut sapien interdum, id rutrum est vulputate. Aliquam vestibulum eu erat id facilisis. Maecenas nulla nibh, iaculis ut sodales ut, rhoncus a urna.</p>
+        <div className="flex flex-col gap-4 items-center justify-center h-screen">
+          <h3 className="bg-purple-200 px-5 py-2.5 text-md text-white-100 font-semibold rounded-md text-center w-fit">Panduan</h3>
+          <div className="gap-4 text-center">
+            <h1 className="text-black-900 text-2xl font-semibold">Ikuti Panduan untuk Mencari dan Mengajukan Event</h1>
+            <p className="text-black-900 text-sm">
+            Langkah-langkah untuk mencari dan mengajukan event di lingkungan FT UGM cukup mudah dilakukan hanya dengan beberapa langkah saja untuk mencari seluruh event atau mengajukan event.
+            </p>
+          </div>
+          <div className="flex flex-row gap-4">
+            <h1 className="text-black-900 font-semibold">Panduan untuk mencari event dan mendaftarkan diri pada suatu event:</h1>
+            <p className="text-black-900">
+              1. Masuk ke akun Anda atau registrasi akun jika belum memiliki akun.
+              <br/>
+              2. Klik “Cari Event” pada bagian navbar atau pada landing page.
+              <br/>
+              3. Seluruh event yang telah diajukan pada platform ini akan muncul. Anda juga bisa menggunakan fitur search untuk melihat event berdasarkan kata kunci yang Anda diberikan.
+              <br/>
+              4. Jika ingin mendaftar pada suatu event,klik salah satu event kemudian klik tombol “Daftar”. Anda juga bisa melihat informasi detail event tersebut.
+              <br/>
+              5. Isi form yang tersedia dan klik tombol “Daftar” untuk mengumpulkan form.
+            </p>
+          </div>
+          <div className="flex flex-row gap-4">
+            <h1 className="text-black-900 font-semibold">Panduan untuk mengajukan suatu event:</h1>
+            <p className="text-black-900">
+              1. Masuk ke akun Anda atau registrasi akun jika belum memiliki akun.
+              <br/>
+              2. Klik “Ajukan Event” pada bagian navbar atau pada landing page.
+              <br/>
+              3. Isi form yang tersedia kemudian klik tombol “Ajukan” untuk mengajukan event.
+            </p>
+          </div>
+        </div>
       </main>
     );
   }
