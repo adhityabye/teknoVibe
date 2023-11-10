4e4935e507f3c6b1e4f220edf886026673b44153
Nantapranawa <pranawananta21@gmail.com>
2023-11-10 09:32:01 +0700

    feat: add search event page

---
+++ frontend/src/app/search/page.jsx
@@ -0,0 +1,163 @@
+
+
+export default function SearchEvent(){
+    return (
+        <section class="searchEvent flex-wrap flex justify-center items-center mt-5">
+        <div class="maincards flex space-x-8 ">
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-x" src="./pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        {/* <a role='button' href='#' class="text-purple-700 bg-transparent px-5 py-1 border border-black rounded-3xl hover:bg-gray-200 bottom-2 right-10">Edit</a> */}
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="maincards flex space-x-8 ">
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        {/* <a role='button' href='#' class="text-purple-700 bg-transparent px-5 py-1 border border-black rounded-3xl hover:bg-gray-200 bottom-2 right-10">Edit</a> */}
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+            
+            <div class="w-60 p-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl">
+                
+                <img class="h-40 object-cover rounded-xl" src="/pewdstream.jpg" alt=""/>
+                    <div class="p-2">
+                        
+                        <h2 class="font-bold text-lg mb-2 ">Event name</h2>
+                        
+                        <p class="text-sm text-gray-600">Event description on date, place, time, target, etc ...</p>
+                    </div>
+                
+                <div>    
+                    <div class="flex space-x-2">
+                        <a role='button' href='#' class="text-white bg-darkedit px-5 py-1 rounded-3xl hover:bg-black">Daftar</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    );
+  }
+
+
