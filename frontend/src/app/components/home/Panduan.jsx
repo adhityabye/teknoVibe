f5b9402bba26a99936fe28b8818a9e832443c1c7
KhairunNisaZ <khairun.nisa.zuqri92@gmail.com>
2023-11-16 12:49:08 +0700

    style: update layout

--- frontend/src/app/components/home/Panduan.jsx
+++ frontend/src/app/components/home/Panduan.jsx
@@ -15,40 +15,41 @@ export default function Panduan() {
             mencari seluruh event atau mengajukan event.
           </p>
         </div>
-        <div className="flex flex-row gap-4">
-          <div className="flex flex-col gap-28">
-            <h1 className="text-black-900 font-semibold text-left">
-              Panduan untuk mencari event dan mendaftarkan diri pada suatu event:
-            </h1>
-            <h1 className="text-black-900 font-semibold text-left">
-              Panduan untuk mengajukan suatu event:
-            </h1>
+        <div className="flex flex-col gap-4 p-10">
+          <div className="flex flex-row gap-10 justify-between">
+            <div className="w-1/2 flex-shrink-0">
+              <h1 className="text-black-900 font-semibold text-left">
+                Panduan untuk mencari event dan mendaftarkan diri pada suatu event:
+              </h1>
+            </div>
+            <div className="text-black-900 w-1/2">
+              <ol className="list-outside list-decimal">
+                <li>Masuk ke akun Anda atau registrasi akun jika belum memiliki akun.</li>
+                <li>Klik “Cari Event” pada bagian navbar atau pada landing page.</li>
+                <li>Seluruh event yang telah diajukan pada platform ini akan muncul.
+                    Anda juga bisa menggunakan fitur search untuk melihat event
+                    berdasarkan kata kunci yang Anda diberikan.</li>
+                <li>Jika ingin mendaftar pada suatu event,klik salah satu event
+                    kemudian klik tombol “Daftar”. Anda juga bisa melihat informasi
+                    detail event tersebut.</li>
+                <li>Isi form yang tersedia dan klik tombol “Daftar” untuk
+                    mengumpulkan form.</li>
+              </ol> 
+            </div>
           </div>
-          <div className="flex flex-col gap-4">
-            <p className="text-black-900">
-              1. Masuk ke akun Anda atau registrasi akun jika belum memiliki akun.
-              <br />
-              2. Klik “Cari Event” pada bagian navbar atau pada landing page.
-              <br />
-              3. Seluruh event yang telah diajukan pada platform ini akan muncul.
-              Anda juga bisa menggunakan fitur search untuk melihat event
-              berdasarkan kata kunci yang Anda diberikan.
-              <br />
-              4. Jika ingin mendaftar pada suatu event,klik salah satu event
-              kemudian klik tombol “Daftar”. Anda juga bisa melihat informasi
-              detail event tersebut.
-              <br />
-              5. Isi form yang tersedia dan klik tombol “Daftar” untuk
-              mengumpulkan form.
-            </p>
-            <p className="text-black-900">
-              1. Masuk ke akun Anda atau registrasi akun jika belum memiliki akun.
-              <br />
-              2. Klik “Ajukan Event” pada bagian navbar atau pada landing page.
-              <br />
-              3. Isi form yang tersedia kemudian klik tombol “Ajukan” untuk
-              mengajukan event.
-            </p>
+          <div className="flex flex-row gap-10 justify-between">
+            <div className="w-1/2 flex-shrink-0">
+              <h1 className="text-black-900 font-semibold text-left">
+                Panduan untuk mengajukan suatu event:
+              </h1>
+            </div>
+            <div className="text-black-900 w-1/2">
+              <ol className="list-outside list-decimal">
+                <li>Masuk ke akun Anda atau registrasi akun jika belum memiliki akun.</li>
+                <li>Klik “Ajukan Event” pada bagian navbar atau pada landing page.</li>
+                <li>Isi form yang tersedia kemudian klik tombol “Ajukan” untuk mengajukan event.</li>
+              </ol> 
+            </div>
           </div>
         </div>
       </div>
