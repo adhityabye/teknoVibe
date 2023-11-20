2188b9fcfb8b7a8dccbf9b46967300a069e496b7
adhityabye <mr.adhityabayu@gmail.com>
2023-11-20 20:50:29 +0700

    changing edit page layout

--- frontend/src/app/editEvent/page.jsx
+++ frontend/src/app/editEvent/page.jsx
@@ -3,57 +3,57 @@ import Navbar from '../components/Navbar';
 export default function EditEvent() {
 
   return (
-    
     <div>
       <Navbar/>
       <main className="flex justify-center">
+        <div>
+          <h1 className="text-2xl font-bold text-center mt-20 mb-10">Edit Detail Event</h1>
+          <form className="flex flex-wrap -mx-3">
+            <div className="w-full md:w-1/2 px-3 mb-5">
+              <div className="flex flex-wrap -mx-3 mb-5">
+                <div className="w-full px-3 mb-5">
+                  <input 
+                    type="text"
+                    color = "#EAEAEA"
+                    placeholder="Nama Event"
+                    className="p-3 bg-gray-input border border-gray-200 rounded w-full" 
+                  />
+                </div>
+                <div className="w-full px-3 mb-5">
+                  <input
+                    type="text"
+                    placeholder="Tanggal Penutupan Pendaftaran"
+                    className="p-3 bg-gray-input border border-gray-200 rounded w-full" 
+                  />
+                </div>
+                <div className="w-full px-3 mb-5">
+                  <textarea
+                    placeholder="Event Description"
+                    className="p-3 bg-gray-input border border-gray-200 rounded w-full"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
 
-      {/* className="bg-white p-8 rounded-lg shadow-md w-3/4" */}
-      <div>
+            <div className="w-full md:w-1/2 px-3 mb-5">
+              <div className="flex flex-wrap -mx-3">
+                <div className="w-full px-3 mb-5">
+                  <select className="p-3 bg-gray-input border border-gray-200 rounded w-full">
+                    <option>Pilihan Divisi</option>
+                    <option>Acara</option>
+                    <option>Perlengkapan</option>
+                  </select>
+                </div>
+                <div className="w-full px-3 mb-5">
+                  <textarea
+                    placeholder="Terms & Conditions"
+                    className="p-3 bg-gray-input border border-gray-200 rounded w-full"
+                  ></textarea>  
+                </div>
+              </div>
+            </div>
 
-        <h1 className="text-2xl font-bold text-center mb-10">Edit Event</h1>
-
-        <form className="flex flex-wrap -mx-3">
-        
-          <div className="w-full md:w-1/2 px-3 mb-5">
-            <input 
-              type="text" 
-              placeholder="Event Name"
-              className="p-3 border border-gray-200 rounded w-full" 
-            />
-          </div>
-
-          <div className="w-full md:w-1/2 px-3 mb-5">  
-            <input
-              type="text"
-              placeholder="Event Closing"
-              className="p-3 border border-gray-200 rounded w-full" 
-            />
-          </div>
-
-          <div className="w-full md:w-1/2 px-3 mb-5">
-            <select className="p-3 border border-gray-200 rounded w-full">
-              <option>Pilihan Divisi</option>
-              <option>Acara</option>
-              <option>Perlengkapan</option>
-            </select>
-          </div>
-
-          <div className="w-full px-3 mb-5">
-            <textarea
-              placeholder="Event Description"
-              className="p-3 border border-gray-200 rounded w-full"
-            ></textarea>
-          </div>
-
-          <div className="w-full px-3 mb-5">
-            <textarea
-              placeholder="Terms & Conditions"
-              className="p-3 border border-gray-200 rounded w-full"
-            ></textarea>  
-          </div>
-
-          <div className="w-full px-3 mb-5 flex justify-center items-center">
+            <div className="w-full px-3 mb-5 flex justify-center items-center">
               <button
                 className="bg-button-dark text-white py-2 px-4 rounded hover:bg-purple-200"
               >
@@ -65,13 +65,10 @@ export default function EditEvent() {
               >
                 Delete
               </button>
-          </div>
-
-        </form>     
-      </div>
-
-    </main>
+            </div>
+          </form>     
+        </div>
+      </main>
     </div>
   )
-
 }
