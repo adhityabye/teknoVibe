ecd74a213424ada010be40655bd9af17fe26ee11
adhityabye <mr.adhityabayu@gmail.com>
2023-11-23 08:01:45 +0700

    add footer section

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/home/Hero";
 import Tentang from "./components/home/Tentang";
 import Panduan from "./components/home/Panduan";
+import Footer from "./components/Footer";
 
 export default function Home(){
   return (
@@ -10,6 +11,7 @@ export default function Home(){
       <Hero />
       <Tentang />
       <Panduan />
+      <Footer/>
     </main>
   );
 }
