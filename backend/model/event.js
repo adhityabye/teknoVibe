0d928197e7af50cfa3ce7fcb94d45d0b38aa1a54
leleonnn <leonardmars63@gmail.com>
2023-11-24 10:23:56 +0700

    update event API

--- backend/model/event.js
+++ backend/model/event.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const eventSchema = new mongoose.Schema({
   eventName: { type: String, required: true },
   eventDescription: { type: String, required: true },
+  department: { type: String, required: true },
+  eventProfileUrl: { type: String, required: true },
   date: { type: Date, required: true, default: Date.now() },
   divisions: { type: String, required: true },
   deadlineDate: { type: Date, required: true },
