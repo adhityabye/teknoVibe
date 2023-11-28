58663060641121cb9875e6e0f1c1de8b3c190265
Nantapranawa <pranawananta21@gmail.com>
2023-11-28 21:11:43 +0700

    feat: routing detail event page when clicking cards

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,10 +2,12 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/home/Hero";
 import Tentang from "./components/home/Tentang";
 import Panduan from "./components/home/Panduan";
+import Search from "./components/home/Search";
 import Footer from "./components/Footer";
 import AjukanEvent from "./components/home/AjukanEvent";
 
 export default function Home(){
+  console.log(Search);
   return (
     <main>
       <div className="flex flex-col gap-60">
@@ -13,6 +15,7 @@ export default function Home(){
         <Hero />
         <Tentang />
         <Panduan />
+        <Search/>
         <AjukanEvent/>
         <Footer/>
       </div>
