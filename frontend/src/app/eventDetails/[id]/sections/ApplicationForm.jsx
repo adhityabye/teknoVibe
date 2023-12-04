357d71c1008259edeba6631ca6264aafee24f4c6
KhairunNisaZ <khairun.nisa.zuqri92@gmail.com>
2023-12-04 19:55:18 +0700

    feat: add confirmation after submit form

--- frontend/src/app/eventDetails/[id]/sections/ApplicationForm.jsx
+++ frontend/src/app/eventDetails/[id]/sections/ApplicationForm.jsx
@@ -13,6 +13,26 @@ const Button = ({ title }) => {
   );
 };
 
+const Notification = ({ isOpen, onClose, message }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center">
+      <div className="absolute inset-0 bg-white bg-opacity-100 backdrop-blur-md" />
+      <div className="flex flex-col shadow-2xl bg-white p-5 px-10 z-10 place-items-center rounded-lg">
+        <p className="font-bold text-purple-200 text-2xl mb-4">Notice</p>
+        <p>Your data has been successfully inserted</p>
+        <button
+          className="mt-10 p-2 px-4 bg-purple-200 text-white rounded-2xl transition-transform duration-300 transform hover:bg-purple-900 hover:scale-110 active:scale-95"
+          onClick={onClose}
+        >
+          Okay
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default function ApplicationForm({ eventId }) {
   const [applicantInfo, setApplicantInfo] = useState({
     namaLengkap: "",
@@ -38,6 +58,10 @@ export default function ApplicationForm({ eventId }) {
     alasan,
     uploadCV,
   } = applicantInfo;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
+
   const handleChange = ({ target }) => {
     const { value, name } = target;
     setApplicantInfo({ ...applicantInfo, [name]: value });
@@ -52,24 +76,63 @@ export default function ApplicationForm({ eventId }) {
       });
       const _data = await res.json();
       console.log(_data);
+
+      // Show notification after successful submission
+      setNotificationMessage("Your data has been successfully submitted!");
+      setIsModalOpen(true);
     } catch (err) {
       console.error(err);
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    applicantInfo.angkatan = parseInt(angkatan);
-    postApplicantInfo(applicantInfo);
+    applicantInfo.angkatan = parseInt(applicantInfo.angkatan);
+
+    try {
+      const res = await fetch("http://localhost:9090/api/staff", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(applicantInfo),
+      });
+
+      const _data = await res.json();
+      console.log(_data);
+
+      // Reset form fields
+      setApplicantInfo({
+        namaLengkap: "",
+        nim: "",
+        emailUGM: "",
+        programStudi: "",
+        angkatan: "",
+        idLine: "",
+        pilihanDivisi: "",
+        alasan: "",
+        uploadCV: "",
+        eventId: eventId,
+      });
+
+      // Show notification after successful submission
+      setNotificationMessage("Your data has been successfully submitted!");
+      setIsModalOpen(true);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
-    <div className="flex flex-col items-center justify-start h-screen gap-4">
+    <div className="flex flex-col items-center justify-start h-screen gap-8">
       <div className="text-center mb-8">
         <h1 className="text-black-900 text-4xl font-semibold">
           Form Pendaftaran
         </h1>
       </div>
+      <Notification
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        message={notificationMessage}
+      />
       <form className="flex flex-col gap-8" onSubmit={(e) => handleSubmit(e)}>
         <div className="flex flex-row gap-8">
           <div className="flex flex-col gap-4">
