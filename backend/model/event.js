b9672d9737fd7096b3ff52983a92ec92267f7fab
adhityabye <mr.adhityabayu@gmail.com>
2023-12-04 21:59:02 +0700

    change required data

--- backend/model/event.js
+++ backend/model/event.js
@@ -4,13 +4,13 @@ const eventSchema = new mongoose.Schema({
   eventName: { type: String, required: true },
   eventDescription: { type: String, required: true },
   department: { type: String, required: true },
-  eventProfileUrl: { type: String, required: true },
-  date: { type: Date, required: true, default: Date.now() },
+  eventProfileUrl: { type: String, required: false },
+  date: { type: Date, required: false, default: Date.now() },
   divisions: { type: String, required: true },
   deadlineDate: { type: Date, required: true },
   tnc: { type: String, required: true },
-  adminId: { type: String, required: true },
-  open: { type: Boolean, required: true, default: true },
+  adminId: { type: String, required: false },
+  open: { type: Boolean, required: false, default: true },
   registeredParticipants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Staff' }],
 });
 
