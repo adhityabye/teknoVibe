54620be9482eba95bb7db8d33799651d3b1f535c
Nantapranawa <pranawananta21@gmail.com>
2023-12-05 01:42:46 +0700

    fixed button position

--- frontend/src/app/components/home/Search.jsx
+++ frontend/src/app/components/home/Search.jsx
@@ -17,7 +17,6 @@ export default function Search() {
       });
   }, []);
 
-
   return (
     <main id="search">
       <div className="flex justify-center items-center">
@@ -33,7 +32,7 @@ export default function Search() {
 
       <section className="searchEvent flex-wrap flex justify-center items-center mt-5">
         <div className="grid grid-cols-4 md:grid-cols-4 grid-rows-2 gap-8 mx-24 cursor-pointer">
-          {data.slice(0,8).map((rows) => (
+          {data.slice(0, 8).map((rows) => (
             <>
               <a href={`/eventDetails/${rows._id}`} key={rows._id}>
                 <div
@@ -50,7 +49,9 @@ export default function Search() {
                     <h2 className="font-bold text-[19px]">{rows.eventName}</h2>
 
                     <p className="text-sm text-gray-600">
-                    {rows.eventDescription.length > 20 ? `${rows.eventDescription.slice(0,20)}....`: rows.eventDescription}
+                      {rows.eventDescription.length > 20
+                        ? `${rows.eventDescription.slice(0, 20)}....`
+                        : rows.eventDescription}
                     </p>
                     <span className="bg-purple-200 text-white text-xs font-medium me-2 px-2.5 py-0.5 rounded">
                       {rows.department}
@@ -66,15 +67,26 @@ export default function Search() {
           ))}
         </div>
       </section>
-      <div className="mt-8 flex place-content-center">
-<a href={"/search"} class= "text-purple-200 font-bold py-2 px-4 rounded flex items-center justify-center transition-transform duration-300 transform hover:translate-y-1 hover:cursor-pointer">
-  <span class="mr-2 hover:text-black">View More</span>
-  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 " viewBox="0 0 20 20" fill="currentColor">
-    <path fill-rule="evenodd" d="M10 14a1 1 0 01-.707-.293l-4-4a1 1 0 011.414-1.414L10 11.586l3.293-3.293a1 1 0 011.414 1.414l-4 4A1 1 0 0110 14z" clip-rule="evenodd" />
-  </svg>
-</a>
-
-</div>
+      <div className="mt-6 flex place-content-center">
+        <a
+          href={"/search"}
+          class="text-purple-200 font-bold py-2 px-4 rounded flex items-center justify-center transition-transform duration-300 transform hover:translate-y-1 hover:cursor-pointer"
+        >
+          <span class="mr-2 hover:text-black">View More</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 "
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 14a1 1 0 01-.707-.293l-4-4a1 1 0 011.414-1.414L10 11.586l3.293-3.293a1 1 0 011.414 1.414l-4 4A1 1 0 0110 14z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </a>
+      </div>
     </main>
   );
 }
